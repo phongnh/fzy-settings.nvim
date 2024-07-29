ebd145407d35e1c1d56b8606951574001fcd1c52
Phong Nguyen <nhphong1406@gmail.com>
2024-07-29 17:04:53 +0700

    Update: Mon, 29 Jul 2024 17:04:53 +0700

--- lua/fzy_settings/extra.lua
+++ lua/fzy_settings/extra.lua
@@ -124,7 +124,7 @@ function M.boutline()
             if tag then
                 local columns = vim.split(tag, "\t")
                 if #columns > 2 then
-                    local linenr = columns[3]:sub(1, -3)
+                    local linenr = tonumber(columns[3]:sub(1, -3))
                     return vim.trim(vim.api.nvim_buf_get_lines(0, linenr - 1, linenr, false)[1])
                 else
                     return nil
